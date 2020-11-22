96fc7728f0b70bfb33f37cb7e2229f038fa44277
Vitor Pamplona <vitor@vitorpamplona.com>
2020-11-22 11:16:19 -0500

    Improvements to Screen UI

--- src/components/PrintVaccinationProgram.js
+++ src/components/PrintVaccinationProgram.js
@@ -20,6 +20,7 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Icon from '@material-ui/core/Icon';
 import API from '../API';
 import QRCode from 'qrcode.react';
+import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -89,7 +90,7 @@ export default function PrintVaccinationProgram({ context }) {
       <QRCode value={calculateQR(program)} fgColor="#3654DD" size="345" level="H" />
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
-          {calculateQR(program)}
+          Scan the code below to create your own Vaccine Certificate
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
@@ -112,29 +113,7 @@ export default function PrintVaccinationProgram({ context }) {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
+          <Typography variant="body2" paragraph>Content: <Link href={calculateQR(program)}>{calculateQR(program)}</Link></Typography>
         </CardContent>
       </Collapse>
     </Card>
