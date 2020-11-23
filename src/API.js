9fb0cd96f97aa1a5a868f717c1bedf93ceef9067
Vitor Pamplona <vitor@vitorpamplona.com>
2020-11-22 21:12:40 -0500

    showing environment.

--- src/API.js
+++ src/API.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+console.log(process.env.NODE_ENV);
+
 export default axios.create({
-  baseURL: process.env.NODE_ENV !== 'production' ? 
+  baseURL: process.env.NODE_ENV === 'production' ? 
            'https://healthpassport-provider-portal.herokuapp.com/'
            : 'http://localhost:3000'
 });
