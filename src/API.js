6b743699d3ab3c889ca1fe1bb560f421b63165b0
Vitor Pamplona <vitor@vitorpamplona.com>
2020-11-22 21:02:29 -0500

    Connecting to Heroku

--- src/API.js
+++ src/API.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3000/'
+  baseURL: process.env.NODE_ENV !== 'production' ? 
+           'https://healthpassport-provider-portal.herokuapp.com/'
+           : 'http://localhost:3000'
 });
