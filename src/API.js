083810b463a5a63dd96ea64252809ef6a2f5886c
Vitor Pamplona <vitor@vitorpamplona.com>
2020-11-23 20:29:43 -0500

    using the new ssl

--- src/API.js
+++ src/API.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: process.env.NODE_ENV === 'production' ? 
-           'http://healthpassport-api.vitorpamplona.com'
+           'https://healthpassport-api.vitorpamplona.com'
            : 'http://localhost:3000'
 });
